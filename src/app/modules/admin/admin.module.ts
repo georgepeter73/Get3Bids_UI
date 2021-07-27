20af683bed0155f5811d397bcf13413e521b36b7
georgepeter <gpeter@lendgenuity.com>
2021-07-26 20:33:33 -0400

    Admin screen changes

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from "@shared/shared.module";
 
 import { AdminRoutingModule } from "./admin-routing.module";
 import { AdminComponent } from "./page/admin/admin.component";
+import {NbCardModule, NbRouteTabsetModule, NbTabsetModule} from '@nebular/theme';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [AdminRoutingModule, SharedModule]
+  imports: [AdminRoutingModule, SharedModule, NbRouteTabsetModule, NbCardModule, NbTabsetModule]
 })
 export class AdminModule {}
--- src/app/modules/admin/page/admin/admin.component.ts
+++ src/app/modules/admin/page/admin/admin.component.ts
@@ -5,4 +5,16 @@ import { Component } from "@angular/core";
   templateUrl: "./admin.component.html",
   styleUrls: ["./admin.component.css"]
 })
-export class AdminComponent {}
+export class AdminComponent {
+
+  tabs: any[] = [
+    {
+      title: 'Route tab #1',
+      route: '/pages/layout/tabs/tab1',
+    },
+    {
+      title: 'Route tab #2',
+      route: '/pages/layout/tabs/tab2',
+    },
+  ];
+}
