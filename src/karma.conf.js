ad1dd0d87437e00aa62a85a048cb50988cf5def5
georgepeter <gpeter@lendgenuity.com>
2021-07-27 17:09:19 -0400

    Admin screen changes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,10 +27,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
-      ChromeTravisCi: {
-        base: 'Chrome',
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
