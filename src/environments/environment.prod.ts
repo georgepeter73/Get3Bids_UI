e5f9fbd04414461c30eff5be40ff936356c72262
georgepeter <gpeter@lendgenuity.com>
2022-06-01 09:01:16 -0400

    #LOAN-108 -Lock Functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   serverUrl: "/api",
   envName: "PROD",
   API_URL: "https://prodbff.loanhouse.us",
-  LOCKDESK_API_URL : "https://lhposb2bbff1uat-env.eba-ytqkw66v.us-east-1.elasticbeanstalk.com",
+  LOCKDESK_API_URL : "https://lhposb2bbff1-env.eba-vyi3qfia.us-east-1.elasticbeanstalk.com",
   is_taxonomy_static: false,
   is_zip_static: false,
   is_common_service_static: false,
