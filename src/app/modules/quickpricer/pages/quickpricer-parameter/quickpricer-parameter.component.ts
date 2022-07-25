91a07e799477d0d4209b99c9be4a244072d5e52b
georgepeter <gpeter@lendgenuity.com>
2022-07-25 11:51:38 -0400

    #LOAN-148 - QuickPricer Phase 1

--- src/app/modules/quickpricer/pages/quickpricer-parameter/quickpricer-parameter.component.ts
+++ src/app/modules/quickpricer/pages/quickpricer-parameter/quickpricer-parameter.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import {Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {faUser,faDollarSign,faHome} from '@fortawesome/free-solid-svg-icons';
+import {TaxonomyService} from '@data/service/taxonomy.service';
+import {Taxonomy} from '@data/schema/taxonomy';
+import {GlobalService} from '@app/service/global.service';
+import {QuickQuote, QuickQuoteAddress} from '@data/schema/lockdesk';
 
 @Component({
   selector: 'app-quickpricer-parameter',
@@ -10,20 +14,137 @@ import {faUser,faDollarSign,faHome} from '@fortawesome/free-solid-svg-icons';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuickpricerParameterComponent implements OnInit {
-faperson = faUser;
-fadollar = faDollarSign;
-faHome = faHome;
+  faperson = faUser;
+  fadollar = faDollarSign;
+  faHome = faHome;
+  occupancyTaxonomy : Taxonomy;
+  propertyInfoTaxonomy : Taxonomy;
+  incomeDocumentationTaxonomy : Taxonomy;
+  miTypesTaxonomy : Taxonomy;
+  prePaymentPenalty : Taxonomy;
+  purposeTypeTaxonomy : Taxonomy;
+  quickQuote : QuickQuote;
+  address: Object;
+  form = {
+    zipcode: null,
+    city: '',
+    state: '',
+    cityList: [],
+    stateList: [],
+    stateDesc: ''
+  };
+  formattedAddress: string;
+  invalidZipCode = true;
+
   constructor(private router: Router,
               private _location: Location,
+              private taxonomyService: TaxonomyService,
+              private  globalService: GlobalService,
 
   ) {
   }
 
   ngOnInit(): void {
+    this.getTaxonomy();
+     this.globalService.getQuickQuote().subscribe(qq =>
+     {
+       this.quickQuote = qq ;
+       if (qq.quoteAddressDTO) {
+         this.form.zipcode = qq.quoteAddressDTO.zip;
+         this.form.city = qq.quoteAddressDTO.city;
+         this.form.state = qq.quoteAddressDTO.state;
+       }
 
+    })
+
+  }
+  getTaxonomy(){
+
+    this.taxonomyService.getAllTaxonomies().subscribe(taxonomies => {
+      this.occupancyTaxonomy = taxonomies
+        .filter(tax => tax.type === 'QQOccupancyType')
+        .pop();
+      this.propertyInfoTaxonomy = taxonomies
+        .filter(tax => tax.type === 'QQPropertyInfoType')
+        .pop();
+      this.incomeDocumentationTaxonomy = taxonomies
+        .filter(tax => tax.type === 'QQIncomeDocumentationType')
+        .pop();
+      this.miTypesTaxonomy = taxonomies.filter(tax => tax.type === 'QQMITypes').pop();
+      this.prePaymentPenalty = taxonomies
+        .filter(tax => tax.type === 'QQPrepaymentPenaltyTypes')
+        .pop();
+      this.purposeTypeTaxonomy = taxonomies
+        .filter(tax => tax.type === 'QQTransactionType')
+        .pop();
+    });
+
+
+  }
+  getAddress(place: object) {
+    this.address = place['formatted_address'];
+    this.form.city = this.getCity(place);
+    this.form.stateDesc = this.getState(place);
+    this.form.state = this.getState(place);
+    this.form.zipcode = this.getPostCode(place);
+    this.formattedAddress = place['formatted_address'];
+    if (
+      this.form.zipcode !== undefined &&
+      String(this.form.zipcode).length >= 3
+    ) {
+      this.invalidZipCode = false;
+    } else {
+      this.invalidZipCode = true;
+    }
+    if (this.form.zipcode && String(this.form.zipcode).length >= 3) {
+      if (this.form.state) {
+        this.quickQuote.quoteAddressDTO = new QuickQuoteAddress(
+          null,
+          null,
+          this.form.zipcode,
+          this.form.state,
+          null,
+          this.form.city,
+          null,
+          null,
+          null
+        );
+
+
+      }
+    }
+    this.globalService.setQuickQuote(this.quickQuote);
+   }
+  getAddrComponent(place, componentTemplate) {
+    let result;
+
+    for (let i = 0; i < place.address_components.length; i++) {
+      const addressType = place.address_components[i].types[0];
+      if (componentTemplate[addressType]) {
+        result = place.address_components[i][componentTemplate[addressType]];
+        return result;
+      }
+    }
+    return;
+  }
+  getCity(place) {
+    const COMPONENT_TEMPLATE = { locality: 'long_name' },
+      city = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+    return city;
+  }
+  getState(place) {
+    const COMPONENT_TEMPLATE = { administrative_area_level_1: 'short_name' },
+      state = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+    return state;
+  }
+  getPostCode(place) {
+    const COMPONENT_TEMPLATE = { postal_code: 'long_name' },
+      postCode = this.getAddrComponent(place, COMPONENT_TEMPLATE);
+    return postCode;
   }
 
   backClicked($event: MouseEvent) {
+    this.globalService.setQuickQuote(this.quickQuote);
     $event.preventDefault();
     this.router.navigate(['/lockdesk/lockdeskhome'])
   }
--- src/app/modules/quickpricer/quickpricer.module.ts
+++ src/app/modules/quickpricer/quickpricer.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { QuickpricerParameterComponent } from './pages/quickpricer-parameter/quickpricer-parameter.component';
 import {QuickPricerRoutingModule} from '@modules/quickpricer/quickpricer-routing.module';
 import {SharedModule} from '@shared/shared.module';
-import {NbCheckboxModule, NbIconModule, NbInputModule} from '@nebular/theme';
+import {NbCheckboxModule, NbIconModule, NbInputModule, NbSelectModule} from '@nebular/theme';
+import {QuickQuoteService} from '@data/service/quickquote.service';
+import {AutocompleteComponent} from '@modules/quickpricer/component/google-places.component';
 
 
 
 @NgModule({
-  declarations: [QuickpricerParameterComponent],
-    imports: [
-        CommonModule, QuickPricerRoutingModule, SharedModule, NbIconModule, NbInputModule, NbCheckboxModule
-    ]
+  declarations: [QuickpricerParameterComponent, AutocompleteComponent],
+  imports: [
+    CommonModule, QuickPricerRoutingModule, SharedModule, NbIconModule, NbInputModule, NbCheckboxModule, NbSelectModule
+  ],
+  providers: [QuickQuoteService],
 })
 export class QuickpricerModule { }
