1381d7c219dda17c119bf454c5119789d1412e9b
georgepeter <gpeter@lendgenuity.com>
2022-07-25 09:38:11 -0400

    #LOAN-148 - QuickPricer Phase 1

--- src/app/modules/quickpricer/quickpricer.module.ts
+++ src/app/modules/quickpricer/quickpricer.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { QuickpricerParameterComponent } from './pages/quickpricer-parameter/quickpricer-parameter.component';
 import {QuickPricerRoutingModule} from '@modules/quickpricer/quickpricer-routing.module';
 import {SharedModule} from '@shared/shared.module';
-import {NbIconModule, NbInputModule} from '@nebular/theme';
+import {NbCheckboxModule, NbIconModule, NbInputModule} from '@nebular/theme';
 
 
 
 @NgModule({
   declarations: [QuickpricerParameterComponent],
-  imports: [
-    CommonModule, QuickPricerRoutingModule, SharedModule, NbIconModule, NbInputModule
-  ]
+    imports: [
+        CommonModule, QuickPricerRoutingModule, SharedModule, NbIconModule, NbInputModule, NbCheckboxModule
+    ]
 })
 export class QuickpricerModule { }
